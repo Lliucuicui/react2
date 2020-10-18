5be81c23c99f1ce8dfb4032eb9c4df7763b271e7
Lliucuicui <153966095@qq.com>
2020-10-18 20:24:16 +0800

    Signed-off-by: Lliucuicui <153966095@qq.com>

---
+++ build/precache-manifest.27d2e56ab302df1f180a1c79d26ca3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869563354c0529dcb5d522c75e76876c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca082659601f50b30486",
+    "url": "/static/css/main.a3a8b971.chunk.css"
+  },
+  {
+    "revision": "bad6a989448d1a14ecd2",
+    "url": "/static/js/2.abc554aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.abc554aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca082659601f50b30486",
+    "url": "/static/js/main.9afd866e.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
